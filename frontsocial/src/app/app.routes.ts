11ccfca76835c9cf9aed18b851697318a4eb66d1
MaccSob <71sobmaciej@gmail.com>
2026-03-20 23:59:17 +0100

    1

--- frontsocial/src/app/app.routes.ts
+++ frontsocial/src/app/app.routes.ts
@@ -1,8 +1,3 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { Registration } from './registration/registration';
 
-export const routes: Routes = [
-    path: 'signup',
-    Component: Registration
-];
+export const routes: Routes = [];
